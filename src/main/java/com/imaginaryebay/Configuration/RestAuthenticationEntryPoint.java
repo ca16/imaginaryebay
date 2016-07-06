002dd557c0379d8fff98c2345aad5e2dc1dd40aa
ca16 <chloeanastasiades@gmail.com>
2016-07-06 14:51:00 -0700

    ignore login/security stuff, just committing so that I can look back on it

---
+++ src/main/java/com/imaginaryebay/Configuration/RestAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.imaginaryebay.Configuration;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component( "restAuthenticationEntryPoint" )
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException ) throws IOException {
+        response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized" );
+    }
+}
