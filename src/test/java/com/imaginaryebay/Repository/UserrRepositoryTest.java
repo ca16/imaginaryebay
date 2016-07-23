5bc7794f746e79ac1961a848c724b3f67911e323
ca16 <chloeanastasiades@gmail.com>
2016-07-22 19:30:47 -0700

    progress on userrrep testing

--- src/test/java/com/imaginaryebay/Repository/UserrRepositoryTest.java
+++ src/test/java/com/imaginaryebay/Repository/UserrRepositoryTest.java
@@ -3,7 +3,6 @@ package com.imaginaryebay.Repository;
 import com.imaginaryebay.Controller.RestException;
 import com.imaginaryebay.DAO.UserrDao;
 import com.imaginaryebay.Models.Userr;
-import com.sun.tools.classfile.Code_attribute;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -11,7 +10,6 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -19,12 +17,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -39,34 +35,45 @@ public class UserrRepositoryTest {
 
     private UserrRepositoryImpl impl;
 
-    private Userr userr1;
-    private Userr userr2;
-    private Userr newUser;
+    private Userr admin1;
+    private Userr admin2;
+    private Userr nonAdmin1;
 
-    private UserDetails user1;
-    private List<GrantedAuthority> authListAce;
+    private UserDetails admin1UD;
+    private UserDetails admin2UD;
+    private UserDetails nonAdmin1UD;
+
+    private List<GrantedAuthority> authListAdmin;
+    private List<GrantedAuthority> authListNonAdmin;
+
 
     @Before
     public void setUp() throws Exception {
 
         MockitoAnnotations.initMocks(this);
 
-        userr1 = new Userr("a@amail.com", "Ace Aceson", "aaaa");
-        userr2 = new Userr("b@bmail.com", "Bb Beeson", "bbbb");
-        newUser = new Userr("new@email.com", "New User", "newnew");
+        admin1 = new Userr("jackie@gmail.com", "Jackie Fire", "dragon", true);
+        admin2 = new Userr("tom@hotmail.com", "Tom Doe", "haha", true);
+        nonAdmin1 = new Userr("butters@gmail.com", "Butters Stotch", "Stotch");
 
-        authListAce = new ArrayList<>();
-        authListAce.add(new SimpleGrantedAuthority("USER"));
-        authListAce.add(new SimpleGrantedAuthority("ADMIN"));
+        authListAdmin = new ArrayList<>();
+        authListAdmin.add(new SimpleGrantedAuthority("USER"));
+        authListAdmin.add(new SimpleGrantedAuthority("ADMIN"));
 
-        UserDetails user1 = new User("a@amail.com", "aaaa", true, true, true, true, authListAce);
+        authListNonAdmin = new ArrayList<>();
+        authListNonAdmin.add(new SimpleGrantedAuthority("USER"));
 
-        when(userrDao.getUserrByID(1L)).thenReturn(userr1);
-        when(userrDao.getUserrByID(2L)).thenReturn(userr2);
+        admin1UD = new User("jackie@gmail.com", "dragon", authListAdmin);
+        admin2UD = new User("tom@hotmail.com", "haha", authListAdmin);
+        nonAdmin1UD = new User("butters@gmail.com", "Stotch", authListNonAdmin);
 
-        when(userrDao.getUserrByEmail("a@amail.com")).thenReturn(userr1);
-        when(userrDao.getUserrByEmail("b@bmail.com")).thenReturn(userr2);
-        when(userrDao.getUserrByEmail("newemail@emails.com")).thenReturn(null);
+        when(userrDao.getUserrByID(1L)).thenReturn(admin1);
+        when(userrDao.getUserrByID(2L)).thenReturn(admin2);
+        when(userrDao.getUserrByID(3L)).thenReturn(nonAdmin1);
+
+        when(userrDao.getUserrByEmail("jackie@gmail.com")).thenReturn(admin1);
+        when(userrDao.getUserrByEmail("tom@hotmail.com")).thenReturn(admin2);
+        when(userrDao.getUserrByEmail("butters@gmail.com")).thenReturn(null);
 
         impl = new UserrRepositoryImpl();
         impl.setUserrDao(userrDao);
@@ -76,14 +83,14 @@ public class UserrRepositoryTest {
     @Test
     public void createNewUserr() throws Exception {
 
-        impl.createNewUserr(newUser);
-        verify(userrDao).persist(newUser);
+        impl.createNewUserr(nonAdmin1);
+        verify(userrDao).persist(nonAdmin1);
 
         try {
-            impl.createNewUserr(userr1);
+            impl.createNewUserr(admin1);
         }catch(RestException exc){
             Assert.assertEquals("User cannot be created.", exc.getMessage());
-            Assert.assertEquals("User with email a@amail.com already exists.", exc.getDetailedMessage());
+            Assert.assertEquals("User with email jackie@gmail.com already exists.", exc.getDetailedMessage());
             Assert.assertEquals(exc.getStatusCode(), HttpStatus.BAD_REQUEST);
         }
 
@@ -91,31 +98,47 @@ public class UserrRepositoryTest {
 
     @Test(expected = RestException.class)
     public void testCreateExistingUser() throws Exception{
-        impl.createNewUserr(userr1);
+        impl.createNewUserr(admin1);
 
     }
 
     @Test
     public void getUserrByID() throws Exception {
 
-        /*
-        Assert.assertEquals(userr1, impl.getUserrByID(1L));
-        Assert.assertEquals(userr2, impl.getUserrByID(2L));
-        */
+        Authentication authAdmin1 = new UsernamePasswordAuthenticationToken(admin1UD, "dragon", authListAdmin);
+        Authentication authAdmin2 = new UsernamePasswordAuthenticationToken(admin2UD, "haha", authListAdmin);
+        Authentication authNonAdmin1 = new UsernamePasswordAuthenticationToken(nonAdmin1UD, "Stotch", authListNonAdmin);
 
-        Authentication auth = new UsernamePasswordAuthenticationToken(user1, "aaaa", authListAce);
-        SecurityContextHolder.getContext().setAuthentication(auth);
-        Assert.assertEquals(userr1, impl.getUserrByID(1L));
+        SecurityContextHolder.getContext().setAuthentication(authAdmin1);
+        Assert.assertEquals(admin1, impl.getUserrByID(1L));
 
+        SecurityContextHolder.getContext().setAuthentication(authAdmin2);
+        Assert.assertEquals(admin1, impl.getUserrByID(1L));
 
+        SecurityContextHolder.getContext().setAuthentication(authNonAdmin1);
+        try {
+            Assert.assertEquals(admin1, impl.getUserrByID(1L));
+        }catch (RestException exc){
+            Assert.assertEquals("Not available.", exc.getMessage());
+            Assert.assertEquals("You do not have the authority to view this user.", exc.getDetailedMessage());
+            Assert.assertEquals(exc.getStatusCode(), HttpStatus.FORBIDDEN);
+        }
+
+    }
+
+    @Test(expected = RestException.class)
+    public void testNonAdminAccessingOtherUser() throws Exception{
+        Authentication authNonAdmin1 = new UsernamePasswordAuthenticationToken(nonAdmin1UD, "Stotch", authListNonAdmin);
+        SecurityContextHolder.getContext().setAuthentication(authNonAdmin1);
+        Assert.assertEquals(admin1, impl.getUserrByID(1L));
     }
 
     @Test
     public void getUserrByEmail() throws Exception {
 
         /*
-        Assert.assertEquals(userr1, impl.getUserrByEmail("a@amail.com"));
-        Assert.assertEquals(userr2, impl.getUserrByEmail("b@bmail.com"));
+        Assert.assertEquals(admin1, impl.getUserrByEmail("a@amail.com"));
+        Assert.assertEquals(admin2, impl.getUserrByEmail("b@bmail.com"));
         */
     }
 
