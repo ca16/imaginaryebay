a1dcca106ef5e40fc1bc96454bf27caafbce901b
Brian Gillespie <bng1290@gmail.com>
2016-08-09 17:05:56 -0700

    minor tweaks to biddings

--- src/main/webapp/resources/js/Item/Item.controller.js
+++ src/main/webapp/resources/js/Item/Item.controller.js
@@ -10,21 +10,18 @@ function itemController($scope, $routeParams, $http, UserService){
         var itemId = $routeParams.itemId;
         var itemCollectionUrl = "/item/";
         var biddingCollectionUrl = "/bidding";
+        var oneDayInMilliseconds = 86400000;
 
         $scope.newbidprice = 0;
-        $scope.validBid = false;
-        $scope.invalidBid = false;
-        $scope.bidMessage = "";
         $scope.currentUser = UserService.returnUser();
         $scope.loggedInFlag = $scope.currentUser != null;
 
-        // $scope.loggedInFlag = true;
 
         // Enable disqus comments
         $scope.disqusConfig = {
             disqus_shortname: 'guarded-journey-11719-herokuapp-com',
             disqus_identifier: 'auction-item' + itemId,
-            disqus_url: 'localhost:8080/app/item/' + itemId
+            disqus_url: 'localhost:8081/app/item/' + itemId
         };
 
         // Configure slideshow for item's pictures
@@ -35,44 +32,47 @@ function itemController($scope, $routeParams, $http, UserService){
         $http.get(itemCollectionUrl + itemId).success(function(data){
             $scope.item = data;
             $scope.slides = data.itemPictures;
+            // Determines if user can edit this item
+            $scope.userCanEdit = $scope.loggedInFlag && $scope.currentUser.id === data.userr.id;
             // Used to determine color for auction countdown. 
             // If less than a day, display in red
-            console.log(data.endtime - (Math.floor(Date.now())));
-            $scope.auctionEndSoon = (data.endtime - (Math.floor(Date.now()))) <= 86400000;
+            var remainingTime = (data.endtime - (Math.floor(Date.now())));
+            $scope.auctionEndSoon = remainingTime <= oneDayInMilliseconds;
+            $scope.auctionOver = remainingTime <= 0;
         });
 
         // POSTs a Bidding with the specified price
         $scope.makeBid = function(){
             var bidPrice = $scope.newbidprice;
-            
             $http.post(biddingCollectionUrl + '/itemID/' + itemId + '/price/' + bidPrice)
                 .success(function(){
                     // Reload the item to display new bid
                     $http.get(itemCollectionUrl + itemId)
                         .success(function(data){
                             $scope.item = data;
-                            bidResult('Bid successful!', data);
+                            bidResult(data)
                         })
                         .error(function(data, status){
-                            bidResult('Error reloading item.', data, status);
+                            bidResult(data, status)
                         });
                 })
                 .error(function(data, status) {
-                    bidResult('Invalid bid, please try again.', data, status);
+                    bidResult(data, status)
                 })
         };
-
-        // Helper function for dealing with bid alerts
-        var bidResult = function(message, data, status){
+        
+        // Handle the response from a posting a Bid
+        // If a status code is found, show invalid bid warning
+        // Else, show bid success message
+        var bidResult = function(data, status){
             if (status){
-                console.error(message, status, data);
-                $scope.invalidBid = true;
+                console.error(data, status);
                 $scope.validBid = false;
-            } else {
-                $scope.invalidBid = false;
+                $scope.invalidBid = true;                
+            }else {
                 $scope.validBid = true;
+                $scope.invalidBid = false;                
             }
-            $scope.bidMessage = message;
         };
     };
 
