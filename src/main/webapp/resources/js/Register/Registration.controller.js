43b7263cb2049730894416ff4bd05a7087cd5fee
BenWng <wnghan@gmail.com>
2016-08-11 21:45:48 -0700

    Fix the bug in registration controller

--- src/main/webapp/resources/js/Register/Registration.controller.js
+++ src/main/webapp/resources/js/Register/Registration.controller.js
@@ -24,15 +24,36 @@ function registrationController($scope,UserService,$location,$http){
             
             $http.post("/user/new",credential)
                 .then(
+                   function() {
+                       return $http({
+                           method: 'POST',
+                           url: "/login",
+                           headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                           transformRequest: function (obj) {
+                               var str = [];
+                               for (var p in obj)
+                                   str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                               return str.join("&");
+                           },
+                           data: {username: $scope.email, password: $scope.password}
+                       })
+                   }
+                )
+                .then(
+                    function(res) {
+                        return $http.get("/user/email/"+$scope.email+"/");
+                    },
+                    function(rej){
+                        window.alert("Username or email address has already existed");
+                    }
+                )
+                .then(
                     function(res){
                         var user=res.data;
                         UserService.setUser(user);
                         window.alert("You are successfully registered, a confirmation email has been sent to you")
                         $location.path("/app/home");
 
-                    },
-                    function(rej){
-                        window.alert("Username or email address has already existed");
                     }
                 )
         }
